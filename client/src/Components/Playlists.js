b08d9905107375991f228dafd6acf4d4883c71ba
Alex <atscott@ucsc.edu>
2020-09-29 10:36:34 -0700

    Seperated Comps

--- client/src/Components/Playlists.js
+++ client/src/Components/Playlists.js
@@ -4,13 +4,49 @@ import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
 
+
 class Playlists extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			dispPlaylist: false,
+			loggedIn: true,
+			playlist_json: {}
+		}
+	}
+
+	getPlaylists(){
+		spotifyApi.getUserPlaylists()
+		.then((response) => {
+			this.playlist_json = response;
+			console.log(this.playlist_json);
+			console.log(this.playlist_json.items[0].name);
+			this.setState({playlists: response.items[0].name,
+						   dispPlaylist: true
+			});
+			this.state.dispPlaylist = true;
+		})
+	}
+
 	render() {
-		return (
-			<div>
-				<h3 style={{color: 'white'}}> We need playlists here ❤️</h3>
-			</div>
-		);
+		if (!this.state.dispPlaylist){
+			return (
+				<div>
+					<h1>Playlist List ;p</h1>
+					{this.state.loggedIn && 
+						<button onClick={() => this.getPlaylists()}> 
+						get the playlists
+						</button>
+					}
+				</div>
+			);
+		} else{
+			return(
+				<div>
+					{this.playlist_json.items[0].name}
+				</div>
+			)
+		}
 	}
 }
 
